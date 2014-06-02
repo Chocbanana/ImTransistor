86cfd0f07d3ceaa9c3387a3814142ca413fe649c
Banana <Bhavana@Bhavana-Jonnalagaddas-MacBook-3.local>
2014-06-02 11:23:45 -0700

    First working version :)

--- IT_main.cpp
+++ IT_main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cstdio>
+#include <cstdlib>
 #include <vector>
 #include <string>
 
@@ -11,30 +12,18 @@ using namespace cv;
 using namespace std;
 using namespace boost::filesystem;
 
-vector<Mat> create_transition(Mat im1, Mat im2, int num);
+void get_images(int argc,  char** argv, Mat *src1, Mat *src2);
+vector<Mat> gradient_transition(Mat im1, Mat im2, int num);
+vector<Mat> move_transition(Mat im1, Mat im2, int num);
+
+
 
 // The function takes in 2 image names as command-line arguments, then asks for
 // the number of frames. It ouputs a series of images as the transition.
-int main( int argc,  char** argv ) {
-  if (argc != 3) {
-    fprintf(stderr, "ERROR: ImTransistor requires 2 image filenames.\n");
-    return -1;
-  }
+int main( int argc, char** argv ) {
 
-  Mat src1, src2;
-  src1 = imread(argv[1]);
-  src2 = imread(argv[2]);
-
-  // Check if we actually loaded an image.
-  if (src1.empty() || src2.empty()) {
-    fprintf(stderr, "ERROR: Images could not be loaded.\n");
-    return -1;
-  }
-
-  if (src1.size() != src2.size()) {
-    fprintf(stderr, "ERROR: Images are not the same size!.\n");
-    return -1;
-  }
+  Mat src1, src2; // Source images
+  get_images(argc, argv, &src1, &src2); // Does the error checking.
 
   // Get number of frames for transisition.
   int num;
@@ -45,16 +34,16 @@ int main( int argc,  char** argv ) {
     return -1;
   }
 
-  Mat test;
-  Canny(src1, test, 200, 201);
-  imshow("Edges....", test);
-  waitKey(0);
+  // Mat test;
+  // Canny(src1, test, 200, 201);
+  // imshow("Edges", test);
+  // waitKey(0);
+  // destroyWindow("Edges");
+
 
   // Create the transistion frames, including the originals in the frames.
-  vector<Mat> frames(create_transition(src1, src2, num));
-  // Needed cuz vectors are stupid and need iterators :P
-  vector<Mat>::iterator it = frames.begin();
-  it = frames.insert(it, src1);
+  vector<Mat> frames(move_transition(src1, src2, num));
+
 
   // Create a folder for the transistion frame images.
   if (!create_directory(current_path() / "Output")) {
@@ -63,8 +52,7 @@ int main( int argc,  char** argv ) {
   }
 
   // Write the images.
-  int i;
-  for (i = 0; i < frames.size(); i++) {
+  for (int i = 0; i < frames.size(); i++) {
     char filename[99];
     sprintf(filename, "Output/frame%d.jpg", i);
     if (!imwrite(filename, frames.at(i))) {
@@ -73,7 +61,7 @@ int main( int argc,  char** argv ) {
     }
   }
 
-  // Ask is a video should be created.
+  // Ask if a video should be created.
   char ifyes;
   cout << "Create video along with frames (it might take a while) (currently "
        << "only works on a Mac)?\n"
@@ -81,17 +69,17 @@ int main( int argc,  char** argv ) {
   cin.clear();
   cin >> ifyes;
 
-  // Create a video from the frames if yes.
+  // Write the video from the frames if yes.
   if ((ifyes == 'Y') || (ifyes == 'y')) {
     cout << "Creating video...\n";
 
     VideoWriter outputVideo("Output/Output Video.mov", 
-                            CV_FOURCC('m','p','4','v'), 20, src1.size(), true);
+                            CV_FOURCC('m','p','4','v'), 30, src1.size(), true);
     if (!outputVideo.isOpened()) {
       fprintf(stderr, "ERROR: Video couldn't be created.\n");
       return -1;
     }
-    for (i = 0; i < frames.size(); i++) {
+    for (int i = 0; i < frames.size(); i++) {
       outputVideo.write(frames.at(i));
     }
     outputVideo.release();
@@ -105,8 +93,44 @@ int main( int argc,  char** argv ) {
 }
 
 
-// Made the frames for a blend transistion.
-vector<Mat> create_transition(Mat im1, Mat im2, int num) {
+
+
+
+
+
+void get_images(int argc,  char** argv, Mat *src1, Mat* src2) {
+
+  if (argc != 3) {
+    fprintf(stderr, "ERROR: ImTransistor requires 2 image filenames.\n");
+    exit(0);
+  }
+
+  *src1 = imread(argv[1], 0);
+  *src2 = imread(argv[2], 0);
+
+  // Check if we actually loaded an image.
+  if (src1->empty() || src2->empty()) {
+    fprintf(stderr, "ERROR: Images could not be loaded.\n");
+    exit(0);
+  }
+
+  if (src1->size() != src2->size()) {
+    fprintf(stderr, "ERROR: Images are not the same size!.\n");
+    exit(0);
+  }
+
+  // Use threshold to make (inverted) binary images, so that the operations
+  // can be performed on the lesser amount of pixels (since black is 0, 
+  // without inverting, the transistion would happen on the white pixels!).
+  threshold(*src1, *src1, 10, 255, THRESH_BINARY_INV);
+  threshold(*src2, *src2, 10, 255, THRESH_BINARY_INV);
+
+}
+
+
+
+// Makes the frames for a gradient transistion.
+vector<Mat> gradient_transition(Mat im1, Mat im2, int num) {
   vector<Mat> frames(num);
   int i;
   double alpha, beta, step;
@@ -116,6 +140,139 @@ vector<Mat> create_transition(Mat im1, Mat im2, int num) {
     beta += step;
     alpha = 1.0 - beta;
     addWeighted(im1, alpha, im2, beta, 0.0, frames.at(i));
+  }
+
+  // Needed cuz vectors are stupid and need iterators :P
+  vector<Mat>::iterator it = frames.begin();
+  it = frames.insert(it, im1);
+
+  return frames;
+}
+
+
+
+// Rudimentary animated transistion.
+vector<Mat> move_transition(Mat im1, Mat im2, int num){
+  // Frames contains all the frames; the Point vectors contain the indices to
+  // the initial and final images, along with arbitrary intermediary.
+  vector<Mat> frames(num);
+  vector<Point> finalv, initv, pixels;
+  // The mat image that has the pixels that will not change.
+  Mat same;
+  double dist;
+
+  bitwise_and(im1, im2, same); // Finds the pixels that are the same.
+
+  // Get non-zero indices.
+  findNonZero(im1 - same, initv);
+  findNonZero(im2 - same, finalv);
+
+  // If initial image has more pixels. VERY CONVOLUTED! 
+  // TODO: Simplify and make more clear of a process :P
+  if (initv.size() > finalv.size()) {
+    pixels.resize(finalv.size(), Point(0,0)); // make proper size
+
+    for (int i = 0; i < finalv.size(); i++) {
+      int indice;
+      dist = 1000;
+
+      // Find the point which has the least distance.
+      for (int j = 0; j < initv.size(); j++) { 
+        if (norm(finalv.at(i) - initv.at(j)) < dist) {
+          indice = j;
+          dist = norm(finalv.at(i) - initv.at(j));
+        }
+      }
+
+      // Add point to pixels vector and delete from original vector.
+      pixels.at(i) = initv.at(indice);
+      initv.erase(initv.begin() + indice);
+    }
+
+    // Get the distances to travel, stored as points. Can be negative!
+    subtract(finalv, pixels, finalv);
+    // The vector to use in the loop; is updated every time with new
+    // coordinates at which to draw the pixels for that frame.
+    vector<Point> move;
+    // The Mat for the extra pixels not used in the initial image. First draws
+    // all of them, then deletes pixels in the loop.
+    Mat extra = Mat::zeros(im1.size(), CV_8UC1);
+    for (int j = 0; j < initv.size(); j++)
+      extra.at<uchar>(initv.at(j)) = 255;
+    int count = 0;
+
+    // Create the frames.
+    for (int i = 0; i < frames.size(); i++) { 
+      // Scale the distance by the step amount.
+      scaleAdd(finalv, (i + 1.0)/(double)num, pixels, move);
+      // Make frame initially blank.
+      frames.at(i) = Mat::zeros(im1.size(), CV_8UC1);
+      // Draw the pixels.
+      // TODO: See if anything more efficient than for loop for drawing 
+      // the frames?
+      for (int j = 0; j < move.size(); j++)
+        frames.at(i).at<uchar>(move.at(j)) = 255;
+      // Reduce amount of extra pixels being drawn each time.
+      for (; count < (int)((i + 1.0)*initv.size()/(double)num); count++)
+        extra.at<uchar>(initv.at(count)) = 0;
+      // Add the different Mats together. So simple! :D
+      frames.at(i) += same + extra;
+    }
+
+  // If final image has more pixels. EQUALLY VERY CONVOLUTED! 
+  } else {
+
+    pixels.resize(initv.size(), Point(0,0));
+
+    for (int i = 0; i < initv.size(); i++) {
+      int indice;
+      dist = 1000;
+
+      // Find point at least distance.
+      for (int j = 0; j < finalv.size(); j++) { 
+        // norm() calculates distance, works with both Point and Mat.
+        if (norm(finalv.at(j) - initv.at(i)) < dist) {
+          indice = j;
+          dist = norm(finalv.at(j) - initv.at(i));
+        }
+      }
+
+      pixels.at(i) = finalv.at(indice);
+      finalv.erase(finalv.begin() + indice);
+    }
+
+    subtract(pixels, initv, pixels);
+    vector<Point> move;
+
+    // The extra Mat here starts out empty initially.
+    Mat extra = Mat::zeros(im1.size(), CV_8UC1);
+    int count = 0;
+
+    for (int i = 0; i < frames.size(); i++) { 
+      scaleAdd(pixels, (i + 1.0)/(double)num, initv, move);
+
+      frames.at(i) = Mat::zeros(im1.size(), CV_8UC1);
+      for (int j = 0; j < move.size(); j++)
+        frames.at(i).at<uchar>(move.at(j)) = 255;
+
+      // Increase amount drawn each time.
+      for (; count < (int)((i + 1.0)*finalv.size()/(double)num); count++)
+        extra.at<uchar>(finalv.at(count)) = 255;
+
+      frames.at(i) += same + extra;
+    }
+
+  }
+
+  // Needed cuz vectors are stupid and need iterators :P
+  vector<Mat>::iterator it = frames.begin();
+  it = frames.insert(it, im1); // Insert original image in at first.
+
+  for (int i = 0; i < frames.size(); i++) {
+    // Invert the grayscale back to normal.
+    bitwise_not(frames.at(i), frames.at(i));
+    // Convert to color.
+    cvtColor(frames.at(i), frames.at(i), CV_GRAY2RGB);
   }
 
   return frames;
--- README.md
+++ README.md
@@ -7,41 +7,32 @@ by Bhavana Jonnalagadda and Anusha Sinha
 
 ## Version
 
-n/a
+1.0.0
+
+## Description
+
+ImTransistor creates a series of frames (and movie if option selected) between two images. Arguments are 2 images of the same size, and can be mostly any format (except .gif). The program will convert them to black and white, and ask for the number of frames to be created; currently, the movie outputs at 30fps. Make sure there is no folder named “Output” in the same directory!
+
+Will be improved in the future, with features such as a GUI, transition options, and more
+natural/fluid looking transitions. :)
 
 
 ### Milestones
 
 Program can:
-- Read in two same formatted, sized, monocolored images and:
+- Read in two same sized images and:
   * ~~output them straight.~~
-  * convert to animated gif (of 2 images).
+  * ~~convert to monochrome and output.~~
 - Create transition:
-  * of any type (most likely fade transition).
-  * that can have a specified number of intermediate images and speed.
-  * which can be output in animated gif format.
-  * using any algorithm.
-  * using image-deformation algorithm, not modified.
-  * using modified/personally created algorithm.
+  * ~~of any type (fade transition).~~
+  * ~~that can have a specified number of intermediate images and speed.~~
+  * ~~which can be output in .mov format.~~
+  * ~~using any algorithm.~~
+  * ~~using modified/personally created algorithm.~~
 - Have a GUI.
 - Read in different types of images:
   * that are different formats.
-  * that are multiple colors (solid colored).
-  * that are of any colors and gradients (will convert to solid color).
-  * any sizes, any colors, different formats.
+  * that are multiple colors, and keeps colors.
 - Do more? :D
 
 
-
-### Goals
-
-**Basic Features to Aim for:**
-- Given two images (centered, only 2 colors, jpg)
-- it will create simple movement of the lineart from one image to another, optimizing creation/deletion/movement of the lineart color (the color less present).
-- Then, given the length of time (in seconds) and fps, it will make a gif that loops for that length of time and fps.
-
-**Second level:**
-- Can give any type of images (will convert to jpg/ 2 color internally)
-- not centered
-- different options for type of transition (less optimized, include bias for side/direction?)
-- gui
